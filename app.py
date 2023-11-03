5a01a068c21a02802ad6677d7723d9751a8eec37
sumeetkuthari <32229745+sumeetkuthari@users.noreply.github.com>
2023-11-02 22:36:37 -0500

    Changes as per the assignment

--- app.py
+++ app.py
@@ -1,16 +1,63 @@
 # Import libraries
+from flask import Flask, request, redirect, url_for, render_template
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
 # Read operation
+@app.route('/')
+def get_transactions():
+    return render_template('transactions.html', transactions=transactions)
 
 # Create operation
+@app.route('/add', methods=['GET', 'POST'])
+def add_transaction():
+    if request.method == 'GET':
+        return render_template('form.html')
+    
+    if request.method == 'POST':
+        transaction = {
+            'id': len(transactions) + 1,
+            'date': request.form['date'],
+            'amount': request.form['amount']
+        }
+        transactions.append(transaction)
+        return redirect(url_for('get_transactions'))
 
 # Update operation
+@app.route('/edit/<int:transaction_id>', methods=['GET', 'POST'])
+def edit_transaction(transaction_id):
+    if request.method == 'GET':
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                return render_template('edit.html', transaction=transaction)
+            
+    if request.method == 'POST':
+        date = request.form['date']
+        amount = float(request.form['amount'])
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+        return redirect(url_for("get_transactions"))
 
 # Delete operation
+@app.route("/delete/<int:transaction_id>")
+def delete_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            break
+    return redirect(url_for("get_transactions"))
 
 # Run the Flask app
-    
+if __name__ == "__main__":
+    app.run(debug=True)
